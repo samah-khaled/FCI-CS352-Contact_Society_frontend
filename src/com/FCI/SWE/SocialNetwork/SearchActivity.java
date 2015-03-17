b76a201ab012c12cda0dca511afd538ce4e14fa6
samah-khaled <samah20120196@gmail.com>
2015-03-17 02:20:53 +0200

    phase_1c

---
+++ src/com/FCI/SWE/SocialNetwork/SearchActivity.java
@@ -0,0 +1,33 @@
+package com.FCI.SWE.SocialNetwork;
+import com.FCI.SWE.Controllers.Application;
+import com.FCI.SWE.Controllers.UserController;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+public class SearchActivity extends Activity implements OnClickListener{
+	
+	EditText searchEditText;
+	Button searchButton;
+
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		searchEditText = (EditText) findViewById(R.id.search);
+		searchButton = (Button) findViewById(R.id.searchButton);
+		searchButton.setOnClickListener(this);
+
+	}
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		UserController controller = Application.getUserController();
+		controller.search(searchEditText.getText().toString() );
+
+	}
+
+}
